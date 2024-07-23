915f3620bcd5b9d41e2d571f184b57e70e263ae5
belavadi123 <arunbelavadi40@gmail.com>
2024-07-23 23:19:31 +0530

    File added

---
+++ Wrapper.java
@@ -0,0 +1,14 @@
+class Wrapper{
+	
+	Wrapper(){
+		System.out.println("Java consists of wrapper classes");
+	}
+	
+	public static void main(String[] args){
+		new Wrapper();
+		new Wrapper();
+		new Wrapper();
+		new Wrapper();
+		Wrapper wrapper = new Wrapper();
+	}
+}
