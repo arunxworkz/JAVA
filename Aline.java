0d31ad9c1f3d76a417f031e0b15623a1c370b493
belavadi123 <arunbelavadi40@gmail.com>
2024-07-23 22:33:35 +0530

    File add

---
+++ Aline.java
@@ -0,0 +1,16 @@
+class Aline{
+
+	public Aline(){
+		System.out.println("People form another counties are called as Alines");
+	
+	
+	public static void main(String[] args){
+		
+		new Alien();
+		new Alien();
+		new Aline();
+		new Aline();
+		Aline aline = new Aline();
+	}
+	
+}
---
+++ Bullet.java
@@ -0,0 +1,15 @@
+class Bullet{
+	
+	Bullet(){
+		System.out.println("Bullet is shot from a double baralle GUN");
+	}
+	
+	public static void main(Sring[] args){
+		
+		new Bullet();
+		new Bullet();
+		new Buller();
+		new Bullet();
+		Bullte bullet = new Bullet();
+	}
+}
