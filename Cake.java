5dfb8a37ae8aae3d29fa64fdceb773a5b53a2562
belavadi123 <arunbelavadi40@gmail.com>
2024-07-23 23:15:41 +0530

    file added

---
+++ Cake.java
@@ -0,0 +1,14 @@
+class Cake{
+	
+	Cake(){
+		System.out.println("Cake is made up of egg");
+	}
+	
+	public static void main(String[] args){
+		new Cake();
+		new Cake();
+		new Cake();
+		new Cake();
+		Cake cake = new Cake();
+	}
+}
