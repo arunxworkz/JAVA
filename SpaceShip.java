c5b1a45d244003eb8389d32ffdae6572dc947a62
belavadi123 <arunbelavadi40@gmail.com>
2024-07-23 22:23:59 +0530

    File added

---
+++ SpaceShip.java
@@ -0,0 +1,15 @@
+class SpaceShip{
+	
+	SpaceShip(){
+		System.out.println("India launched Chandrayaan-3 on July 14, 2022");
+	}
+	
+	public static void maain(String[] args){
+		
+		new SpaceShip();
+		new SpaceShip();
+		new SpaceShip();
+		new SpaceShip();
+		SpaceShip spaceshop = new SpaceShip();
+	}
+}
