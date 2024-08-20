1474c4db5b64c653bf9f11b8fb89c311b1d0c1f9
belavadi123 <arunbelavadi40@gmail.com>
2024-08-20 10:52:55 +0530

    add file

---
+++ School.java
@@ -0,0 +1,19 @@
+class School{
+	
+	School (String name){
+		System.out.println("School name is: "+name);
+	}
+	
+	public static void main(String[] args){
+		
+		
+		School school = new School("St.Joseph's");
+		
+		
+		
+		
+		
+	}
+	
+	
+}
