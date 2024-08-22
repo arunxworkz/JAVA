b7364cd065ab8f392f54c7cea2a73aee229f6d23
arunxworkz <arunbelavadi30.xworkz@gmail.com>
2024-08-22 23:00:51 +0530

    Update SnadalRunner.java

--- Sandal/src/com/xworkz/Sandal/SnadalRunner.java
+++ Sandal/src/com/xworkz/Sandal/SnadalRunner.java
@@ -10,14 +10,16 @@ public class SnadalRunner {
 		SandalData s = new SandalData("Bata", 'L', 5220.2, "Brown");
 		SandalData s1 = new SandalData("Puma", 'M', 6220.2, "Balck and White");
 		SandalData s2 = new SandalData("Nike", 'M', 6000, null);
+		SandalData s3 = new SandalData("Adidas", 'L', 4000, "Sky Blue");
 		Data d = new Data();
 		
 		/*Data d1 = new Data();
 		d.print();  no necessary of creating of one or more instances of the 'Data' class*/
- 		
+
 		d.store(s);
 		d.store(s1);
 		d.store(s2);
+		d.store(s3);
 		d.print();
 		
 		
