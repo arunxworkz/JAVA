e537e06e5f220f3219255496aa5b247f44896f54
belavadi123 <arunbelavadi40@gmail.com>
2024-09-11 23:03:21 +0530

    Static block

---
+++ ThisandSuper Chaining/Block.java
@@ -0,0 +1,14 @@
+class Block{
+	
+	
+	static{
+		System.out.println("Stati block is executed atleast once");
+	}
+	
+	
+	public static void main(String[] args){
+		Block block = new Block();
+	}
+	
+	
+}
