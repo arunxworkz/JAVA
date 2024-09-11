afe9cd1773b612b34137aea83de2a03a81785a46
belavadi123 <arunbelavadi40@gmail.com>
2024-09-11 20:29:33 +0530

    add file

--- ThisandSuper Chaining/Finalkeyword/src/com/xworkz/finalkeyword/internal/FinalClass.java
+++ ThisandSuper Chaining/Finalkeyword/src/com/xworkz/finalkeyword/internal/FinalClass.java
@@ -1,5 +1,15 @@
 package com.xworkz.finalkeyword.internal;
 
-public class FinalClass {
+public final class FinalClass {
 
+	String sentence;
+	
+	public String method(String sentence) {
+		this.sentence = sentence;
+		System.out.println("This is Final class. Printitg the senetence: "+sentence);
+		return "Today is a good day";
+	}
+	    
+	
+	
 }
